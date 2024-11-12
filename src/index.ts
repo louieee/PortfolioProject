54071c8a7383e30498124007367687177aa58340
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2024-11-13 00:13:07 +0100

    add json data

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
 // src/index.ts
 import './index.css';
 
+import * as fs from 'fs';
+
+
+
+async function fetchJsonFromServer(filePath: string) {
+    const response = await fetch(filePath);
+    const jsonData = await response.json();
+    return jsonData;
+  }
+
+const filePath = 'db.json';
+
+fetchJsonFromServer(filePath)
+  .then(jsonData => {
+    console.log(jsonData); // Use the parsed JSON data
+  })
+  .catch(error => {
+    console.error('Error fetching JSON:', error);
+  });
